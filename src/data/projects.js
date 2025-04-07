cf32530be245bacd9afbeaa3f0744eb6587f5a8d
polivaresc <paulaolca98@gmail.com>
2025-04-07 21:19:37 +0200

    project part started

--- src/data/projects.js
+++ src/data/projects.js
@@ -35,7 +35,7 @@ export const projects = [
     },
     { 
         name: 'This Portfolio', 
-        codeLink: 'https://github.com/Polivaresc/cv-polivaresc', 
+        codeLink: '', 
         siteLink: '',
         p1: 'This is a personal CV website built with React, designed to showcase my background, skills, and projects in a clean and responsive layout.', 
         p2: 'The project focuses on practicing React component structure, reusable UI blocks and responsive design. It includes sections like About Me, Educational Background, Projects, and Contact, and was developed as part of my learning journey.',
